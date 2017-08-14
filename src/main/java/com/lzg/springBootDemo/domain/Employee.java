c6fd2c6e4bce3d7e1d81007b9d2fa8bf6125c1a6
liuzenggang <1172727371@qq.com>
2017-08-14 16:47:10 +0800

    读取配置文件+自定义属性+random.* 属性

--- src/main/java/com/lzg/springBootDemo/domain/Employee.java
+++ src/main/java/com/lzg/springBootDemo/domain/Employee.java
@@ -1,12 +1,28 @@
 package com.lzg.springBootDemo.domain;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
 /**
  * Desc:
  * @Author  Liuzeg
  * @Date  2017/8/7 14:58
  */
+
+/**
+ * 通过 @ConfigurationProperties(prefix = “employ”) 注解，将配置文件中以 employ 前缀的属性值自动绑定到对应的字段中。
+ * 同时用 @Component 作为 Bean 注入到 Spring 容器中。
+ */
+@Component
+@ConfigurationProperties(prefix = "employ")
 public class Employee {
     private String name;
+
+    private String age;
+
+    private String desc;
+
+    private String uuid;
 
     public String getName() {
         return name;
@@ -14,6 +30,40 @@ public class Employee {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", desc='" + desc + '\'' +
+                ", uuid='" + uuid + '\'' +
+                '}';
     }
 }
 
