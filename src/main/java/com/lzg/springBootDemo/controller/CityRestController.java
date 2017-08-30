07d6cd928247d920315aa8c0107a9f185350b1b6
liuzenggang <1172727371@qq.com>
2017-08-30 21:08:52 +0800

    Spring Boot 集成 FreeMarker

--- src/main/java/com/lzg/springBootDemo/controller/CityRestController.java
+++ src/main/java/com/lzg/springBootDemo/controller/CityRestController.java
@@ -1,14 +1,6 @@
 package com.lzg.springBootDemo.controller;
 
-import com.lzg.springBootDemo.domain.City;
-import com.lzg.springBootDemo.service.CityService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * 城市 Controller 实现 Restful HTTP 服务
@@ -17,16 +9,16 @@ import java.util.List;
 @RestController
 public class CityRestController {
 
-    @Autowired
-    private CityService cityService;
-
-    @RequestMapping(value = "api/city",method = RequestMethod.GET)
-    public List<City> findAllCity(){
-         return cityService.findAllCity();
-    }
-
-    @RequestMapping(value = "api/city/{id}",method = RequestMethod.GET)
-    public City findById(@PathVariable(value = "id")Long id){
-        return cityService.findById(id);
-    }
+//    @Autowired
+//    private CityService cityService;
+//
+//    @RequestMapping(value = "api/city",method = RequestMethod.GET)
+//    public List<City> findAllCity(){
+//         return cityService.findAllCity();
+//    }
+//
+//    @RequestMapping(value = "api/city/{id}",method = RequestMethod.GET)
+//    public City findById(@PathVariable(value = "id")Long id){
+//        return cityService.findById(id);
+//    }
 }
